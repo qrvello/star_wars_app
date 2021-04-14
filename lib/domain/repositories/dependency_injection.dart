ae5d9369bcb141578da83e1b75dc1c36ed9e6ec3
Santiago Curvello <santicurvello@gmail.com>
2021-04-14 15:41:16 -0300

    remove dead code

--- lib/domain/repositories/dependency_injection.dart
+++ lib/domain/repositories/dependency_injection.dart
@@ -1,10 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:star_wars_app/data/api_repository_impl.dart';
 import 'package:star_wars_app/data/database_repository_impl.dart';
-import 'package:star_wars_app/domain/cubits/bottom_nav_bar_cubit.dart';
-import 'package:star_wars_app/domain/cubits/list_charactes_cubit/list_characters_offline_cubit.dart';
-import 'package:star_wars_app/domain/cubits/list_charactes_cubit/list_characters_online_cubit.dart';
-
 import 'api_repository.dart';
 import 'database_repository.dart';
 
@@ -12,19 +8,4 @@ final List<RepositoryProvider> repositoryProviders = [
   RepositoryProvider<ApiRepository>(create: (_) => ApiRepositoryImpl()),
   RepositoryProvider<DatabaseRepository>(
       create: (_) => DatabaseRepositoryImpl()),
-];
-
-final List<BlocProvider> cubitsProviders = [
-  BlocProvider(create: (context) => BottomNavBarCubit()),
-  BlocProvider(
-    create: (context) => ListCharactersOnlineCubit(
-      context.read<ApiRepository>(),
-      context.read<DatabaseRepository>(),
-    ),
-  ),
-  BlocProvider(
-    create: (context) => ListCharactersOfflineCubit(
-      context.read<DatabaseRepository>(),
-    ),
-  ),
 ];

5c7aa2dcea9ad01904947d38af713cf1853263e9
Santiago Curvello <santicurvello@gmail.com>
2021-04-14 15:36:15 -0300

    cambios en la organizacion de archivos

--- lib/domain/repositories/dependency_injection.dart
+++ lib/domain/repositories/dependency_injection.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:star_wars_app/data/repositories_impl/api_repository_impl.dart';
-import 'package:star_wars_app/data/repositories_impl/database_repository_impl.dart';
+import 'package:star_wars_app/data/api_repository_impl.dart';
+import 'package:star_wars_app/data/database_repository_impl.dart';
 import 'package:star_wars_app/domain/cubits/bottom_nav_bar_cubit.dart';
 import 'package:star_wars_app/domain/cubits/list_charactes_cubit/list_characters_offline_cubit.dart';
 import 'package:star_wars_app/domain/cubits/list_charactes_cubit/list_characters_online_cubit.dart';
